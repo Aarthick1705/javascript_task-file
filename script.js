bb3fe51af5023d17ca8127895c9827031e48bb67
Aarthick1705 <aarthickraja1705@gmail.com>
2026-08-26 20:34:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var studetname= "Aarthickraja A P";
+var age = 27;
+var mark = 450;
+console.log(studetname);
+console.log(age);
+console.log(mark);
+
+
+let username = prompt("Enter your Name");
+console.log(username);
+
+
+let userage = prompt(" Enter your age");
+console.log(userage);
+
+let question = confirm("Are you eligible for vote");
+console.log(question);
+
+
+username = prompt("Enter your name");
+document. writeln(username);
+
